d26e8a5826025ea03a73dd55cba82e550048aee3
a-kumar5 <ayush.1306.kumar@gmail.com>
2022-07-30 13:09:49 +0530

    Naming convention

--- TwoSums/bruteForce.py
+++ TwoSums/bruteForce.py
@@ -1,10 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         length = len(nums)
-        op = []
+        values = []
         for i in range(length):
             for j in range(i+1,length):
                 if target == nums[i] + nums[j]:
-                    op.append(i)
-                    op.append(j)
-        return op
+                    values.append(i)
+                    values.append(j)
+        return values
