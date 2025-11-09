fcf3c2777d0c501d9f6a8b891948e6581029b4ac
Ayush Kumar <ayush.1306.kumar@gmail.com>
2025-11-09 09:50:51 +0530

    Implement isAnagram and twoSum functions

--- main.go
+++ main.go
@@ -1,20 +1,49 @@
+// You can edit this code!
+// Click here and start typing.
 package main
 
 import "fmt"
 
-func main() {
-	nums := []int{1, 3, 2, 1}
-	fmt.Println(ContainsDuplicate(nums))
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	sCount := make(map[byte]int)
+	tCount := make(map[byte]int)
+
+	for i := 0; i < len(s); i++ {
+		sCount[s[i]]++
+	}
+
+	for j := 0; j < len(t); j++ {
+		tCount[t[j]]++
+	}
+	for k, v := range sCount {
+		if v != tCount[k] {
+			return false
+		}
+	}
+	return true
 }
 
-func ContainsDuplicate(nums []int) bool {
-	mp := map[int]bool{}
-	for _, num := range nums {
-		if mp[num] {
-			return true
+func twoSum(nums []int, target int) []int {
+	indexNums := make(map[int]int)
+	for i, v := range nums {
+		if _, ok := indexNums[target-v]; ok {
+			return []int{indexNums[v], i}
+
 		}
-		mp[num] = true
-		//fmt.Println(mp)
+		indexNums[v] = i
 	}
-	return false
+	return []int{}
+}
+
+func main() {
+	fmt.Println("Hello, 世界")
+	//s := "racecar"
+	//t := "racecar"
+	nums := []int{3, 4, 5, 6}
+	target := 7
+	//fmt.Println(isAnagram(s, t))
+	fmt.Println(twoSum(nums, target))
 }
